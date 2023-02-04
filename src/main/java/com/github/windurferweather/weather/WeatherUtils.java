693fe062b1a6da95a0aec6a2a07d01e6acfad7a6
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-04 17:53:13 +0100

    Add Utils with fields final static for wind speed, temp key, valid date key, city name key, lat key, lon key

---
+++ src/main/java/com/github/windurferweather/weather/WeatherUtils.java
@@ -0,0 +1,10 @@
+package com.github.windurferweather.weather;
+
+class WeatherUtils {
+    public final static String  WIND_SPEED_KEY ="wind_spd";
+    public final static String  TEMP_KEY ="temp";
+    public final static String  VALID_DATE_KEY ="valid_date";
+    public final static String  CITY_NAME_KEY ="city_name";
+    public final static String  LAT_KEY ="lat";
+    public final static String  LON_KEY ="lon";
+}
