f24545013483352cae69fabbfc7c7cb78a7adb7c
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-04 14:50:48 +0100

    Initial commit

---
+++ src/main/java/com/github/windurferweather/WindurferWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.github.windurferweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindurferWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindurferWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/windurferweather/WindurferWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.windurferweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindurferWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
