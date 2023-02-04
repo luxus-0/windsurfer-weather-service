626a4349dd9838f53922778ab7ac6721d4b9b5b7
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-04 17:52:11 +0100

    Add entity class With fields: avgTemp, windSpeed, date

--- src/main/java/com/github/windurferweather/weather/WeatherResponse.java
+++ src/main/java/com/github/windurferweather/weather/WeatherResponse.java
@@ -1,20 +1,26 @@
 package com.github.windurferweather.weather;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.Builder;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Builder
 class WeatherResponse {
     private double avgTemp;
-    private BigDecimal windSpeed;
+    private double windSpeed;
     private LocalDate date;
+
+    @OneToOne
+
+    private CityResponse city;
 
     public WeatherResponse() {
     }
 
-    public WeatherResponse(LocalDate date, double avgTemp, BigDecimal windSpeed) {
+    public WeatherResponse(LocalDate date, double avgTemp, double windSpeed) {
         this.date = date;
         this.avgTemp = avgTemp;
         this.windSpeed = windSpeed;
@@ -28,11 +34,11 @@ class WeatherResponse {
         this.avgTemp = avgTemp;
     }
 
-    public BigDecimal getWindSpeed() {
+    public double getWindSpeed() {
         return windSpeed;
     }
 
-    public void setWindSpeed(BigDecimal windSpeed) {
+    public void setWindSpeed(double windSpeed) {
         this.windSpeed = windSpeed;
     }
 
@@ -42,5 +48,13 @@ class WeatherResponse {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public CityResponse getCity() {
+        return city;
+    }
+
+    public void setCity(CityResponse city) {
+        this.city = city;
     }
 }
