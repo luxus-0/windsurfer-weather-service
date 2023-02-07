d6858f8da1f88270e7170223474cc661c1f57853
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-07 22:51:33 +0100

    Change

---
+++ src/main/java/com/github/windurferweather/weather/WeatherClient.java
@@ -0,0 +1,13 @@
+package com.github.windurferweather.weather;
+
+import org.springframework.web.client.RestTemplate;
+
+class WeatherClient {
+    private final RestTemplate restTemplate;
+
+    WeatherClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
+}
