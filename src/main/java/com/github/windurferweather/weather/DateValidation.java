d83893c67aa3f37bf3b8798af22746747a437eb5
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-07 22:51:29 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/DateValidation.java
+++ src/main/java/com/github/windurferweather/weather/DateValidation.java
@@ -1,9 +1,14 @@
 package com.github.windurferweather.weather;
 
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+@Log4j2
 class DateValidation {
     boolean isValid (String actualDate){
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -11,8 +16,10 @@ class DateValidation {
         try {
             dateFormat.parse(actualDate);
         } catch (ParseException e) {
+            log.info("Incorrect date");
             return false;
         }
+        log.info("Correct date");
         return true;
     }
 }
