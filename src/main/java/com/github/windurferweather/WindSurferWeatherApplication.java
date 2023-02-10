7987466e0f97d632d8b33a9f82d50f90c5d6c621
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-10 20:53:57 +0100

    Rename class Main

--- src/main/java/com/github/windurferweather/WindSurferWeatherApplication.java
+++ src/main/java/com/github/windurferweather/WindSurferWeatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WindurferWeatherApplication {
+public class WindSurferWeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WindurferWeatherApplication.class, args);
+		SpringApplication.run(WindSurferWeatherApplication.class, args);
 	}
 
 }
