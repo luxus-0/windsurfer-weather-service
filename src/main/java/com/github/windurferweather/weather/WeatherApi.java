c6faabe926bb4e907a6c1c0f4bd89475736d0f1e
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-12 21:53:05 +0100

    Add api method: readWindSurferWeatherLocalizedByDate  with argument date

--- src/main/java/com/github/windurferweather/weather/WeatherApi.java
+++ src/main/java/com/github/windurferweather/weather/WeatherApi.java
@@ -2,20 +2,19 @@ package com.github.windurferweather.weather;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class WeatherApi {
 
-    private final WeatherForecastService weatherForecastService;
+    private final WindSurferServiceImpl windSurferServiceImpl;
 
-    WeatherApi(WeatherForecastService weatherForecastService) {
-        this.weatherForecastService = weatherForecastService;
+    WeatherApi(WindSurferServiceImpl windSurferServiceImpl) {
+        this.windSurferServiceImpl = windSurferServiceImpl;
     }
 
-    @GetMapping("/weather")
-    WeatherResponseDto getWeather(@RequestBody WeatherResponse weatherResponse){
-        return weatherForecastService.retrieveWeatherForecast(weatherResponse);
+    @GetMapping("/weather/{date}")
+    WindSurferWeatherDto readWindSurferWeatherLocalizedByDate(@PathVariable String date){
+        return windSurferServiceImpl.readWindSurfingLocationByDate(date);
     }
 }
