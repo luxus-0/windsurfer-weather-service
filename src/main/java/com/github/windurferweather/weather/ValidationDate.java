060682d38d2eeb3787c4d210bf63c68213af0f3f
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-12 21:51:18 +0100

    Add date validation method

---
+++ src/main/java/com/github/windurferweather/weather/ValidationDate.java
@@ -0,0 +1,23 @@
+package com.github.windurferweather.weather;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+@Service
+@Log4j2
+class ValidationDate {
+    boolean isValid(String date) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        df.setLenient(true);
+        try {
+            df.parse(date);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+}
