e729ed5ddba883494de6fbc603ba93f507b72637
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-13 12:41:37 +0100

    Add method that write windsurfer weather location api to CSV file.

--- src/main/java/com/github/windurferweather/weather/WindSurferWeatherApi.java
+++ src/main/java/com/github/windurferweather/weather/WindSurferWeatherApi.java
@@ -1,5 +1,7 @@
 package com.github.windurferweather.weather;
 
+import com.github.windurferweather.weather.io.WindSurferWeatherReader;
+import com.github.windurferweather.weather.io.WindSurferWeatherWriter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 class WindSurferWeatherApi {
 
     private final WindSurferServiceImpl windSurferServiceImpl;
+    private final WindSurferWeatherWriter windSurferWeatherWriter;
 
-    WindSurferWeatherApi(WindSurferServiceImpl windSurferServiceImpl) {
+    WindSurferWeatherApi(WindSurferServiceImpl windSurferServiceImpl, WindSurferWeatherWriter windSurferWeatherWriter) {
         this.windSurferServiceImpl = windSurferServiceImpl;
+        this.windSurferWeatherWriter = windSurferWeatherWriter;
     }
 
     @GetMapping("/weather/{date}")
-    WindSurferWeatherDto readWindSurferWeatherLocalizedByDate(@PathVariable String date){
+    WindSurferWeatherDto readWindSurferWeatherLocalizationByDate(@PathVariable String date){
         return windSurferServiceImpl.readWindSurfingLocationByDate(date);
+    }
+
+    @GetMapping("/weather/write_to_csv_file/{city}/{country}")
+    String writeWeatherFromURLToCSV(@PathVariable String city, @PathVariable String country) throws Exception {
+        return windSurferWeatherWriter.writeFromWindSurferWeatherLocalizationURLToFileCSV(city, country);
     }
 }
