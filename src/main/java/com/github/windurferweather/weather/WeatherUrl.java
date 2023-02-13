88859fbcbe0fd6f2c9a795d741aed5fb6f99dc5f
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-13 13:59:40 +0100

    Add 5 urls for: Jastarnia, brudgetown, fortaleza, pissouri, le_monre_url

---
+++ src/main/java/com/github/windurferweather/weather/WeatherUrl.java
@@ -0,0 +1,9 @@
+package com.github.windurferweather.weather;
+
+public class WeatherUrl {
+    String jastarnia_url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Jastania&country=Poland&key=273c07c4d603455cabd9f94cb58b526c";
+    String bridgetown_url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Bridgetown&country=Barbados&key=273c07c4d603455cabd9f94cb58b526c";
+    String fortaleza_url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Fortaleza&country=Brasil&key=273c07c4d603455cabd9f94cb58b526c";
+    String pissouri_url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Pissouri&country=Cyprus&key=273c07c4d603455cabd9f94cb58b526c";
+    String le_monre_url = "https://api.weatherbit.io/v2.0/forecast/daily?city=Le Monre&country=Mauritius&key=273c07c4d603455cabd9f94cb58b526c";
+}
