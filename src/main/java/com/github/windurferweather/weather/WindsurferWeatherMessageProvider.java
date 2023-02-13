c8e0ae6e81e34d83d5f60ae2e6f944ea3c55c368
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-13 12:44:33 +0100

    Add static method: -String getWeatherUrl(String city, String country) contains all url with city and country with key api

--- src/main/java/com/github/windurferweather/weather/WindsurferWeatherMessageProvider.java
+++ src/main/java/com/github/windurferweather/weather/WindsurferWeatherMessageProvider.java
@@ -1,7 +1,12 @@
 package com.github.windurferweather.weather;
 
-class WindsurferWeatherMessageProvider {
+public class WindsurferWeatherMessageProvider {
+
+    public static String getWeatherUrl(String city, String country){
+        return WEATHER_URL + "daily?" + "city=" + city +"&country=" +country+ "&key=" + API_KEY;
+    }
     public static final String WEATHER_URL = "https://api.weatherbit.io/v2.0/forecast/";
+
     public static final String API_KEY = "273c07c4d603455cabd9f94cb58b526c";
 
     public static final int MIN_TEMP = 3;
