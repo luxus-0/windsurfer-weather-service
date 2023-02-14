bbe645c78065040122f7a9c5210a64fc60518b91
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-14 14:19:58 +0100

    change

--- src/main/java/com/github/windurferweather/weather/WindSurferLocationService.java
+++ src/main/java/com/github/windurferweather/weather/WindSurferLocationService.java
@@ -3,5 +3,5 @@ package com.github.windurferweather.weather;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface WindSurferLocationService {
-    WindSurferWeatherDto readWindSurfingLocationByDate(String date) throws JsonProcessingException;
+    WindSurferWeatherDto readWindSurfingLocationByDate(String date) throws Exception;
 }
