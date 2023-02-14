58d96452a54ecca12f6ebc0e1ca70fccc7408b5a
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-14 14:14:55 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/io/WindSurferWeatherWriter.java
+++ src/main/java/com/github/windurferweather/weather/io/WindSurferWeatherWriter.java
@@ -1,6 +1,5 @@
 package com.github.windurferweather.weather.io;
 
-import com.github.windurferweather.weather.PlaceValidation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -10,34 +9,35 @@ import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 
-import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.*;
+import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.getUrl;
 import static com.github.windurferweather.weather.io.FilePath.FILE_PATH;
-import static com.github.windurferweather.weather.io.WindSurferWeatherReader.readWindSurferWeatherByLocalization;
 
 @Service
 @Log4j2
 public class WindSurferWeatherWriter {
 
 
-    private final PlaceValidation placeValidation;
+    private final WindSurferWeatherReader windSurferWeatherReader;
     private static final StringBuilder windsurferWeatherBuilder = new StringBuilder();
 
-    public WindSurferWeatherWriter(PlaceValidation placeValidation) {
-        this.placeValidation = placeValidation;
+    public WindSurferWeatherWriter(WindSurferWeatherReader windSurferWeatherReader) {
+        this.windSurferWeatherReader = windSurferWeatherReader;
     }
 
-    public String writeFromWindSurferWeatherLocalizationURLToFileCSV(String city, String country) throws Exception {
-        placeValidation.isValid(city, country);
-        String weatherUrl = getWeatherUrl(city, country);
+
+    public String writeFromWindSurferWeatherURLToFileCSV(String datetime) throws Exception {
+        String weatherUrl = getUrl(datetime);
         URL url = new URL(weatherUrl);
         URLConnection connection = url.openConnection();
         connection.setDoOutput(true);
+
         OutputStream outputStream = new FileOutputStream(FILE_PATH);
         OutputStreamWriter out = new OutputStreamWriter(outputStream);
-        String read = readWindSurferWeatherByLocalization(city, country);
-        out.write(read);
-        StringBuilder windsurferWeatherByCityAndCountry = windsurferWeatherBuilder.append(read);
-        log.info(windsurferWeatherByCityAndCountry);
-        return windsurferWeatherByCityAndCountry.toString();
+
+        String readWeather = windSurferWeatherReader.readWindSurferWeatherByDate(datetime);
+        out.write(readWeather);
+        StringBuilder windsurferWeather = windsurferWeatherBuilder.append(readWeather);
+        log.info(windsurferWeather);
+        return windsurferWeather.toString();
     }
 }
