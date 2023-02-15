5c4a88b9dec14000f0a583cc5e107e0ecb63d314
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-16 00:13:41 +0100

    change

---
+++ src/main/java/com/github/windurferweather/weather/WeatherClient.java
@@ -0,0 +1,19 @@
+package com.github.windurferweather.weather;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.API_KEY;
+import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.WEATHER_URL;
+
+@Service
+@Log4j2
+class WeatherClient {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    WeatherResponseDto getWeatherForLocation(Object...objects) {
+        return restTemplate.getForObject(WEATHER_URL + "daily?city={city}&country={country}&key={API_KEY}",
+                WeatherResponseDto.class, objects, API_KEY);
+    }
+}
