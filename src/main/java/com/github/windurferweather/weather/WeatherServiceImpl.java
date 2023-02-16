bf981015816e985fbb89b2e9059dfc2dd68c92a2
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-16 01:01:46 +0100

    change

--- src/main/java/com/github/windurferweather/weather/WeatherServiceImpl.java
+++ src/main/java/com/github/windurferweather/weather/WeatherServiceImpl.java
@@ -2,12 +2,16 @@ package com.github.windurferweather.weather;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 import java.util.Set;
 
 import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.*;
+import static java.util.Objects.requireNonNull;
 
 @Log4j2
 @Service
@@ -28,9 +32,25 @@ class WeatherServiceImpl implements WeatherService {
 
     @Override
     public WeatherResponseDto readWindsurfingLocation(String date) {
-        WeatherResponseDto weatherResponseDto =  windsurferWeatherClient.getWeatherForLocation("Jastarnia","Poland");
+        valid(date);
+        Set<LocationDto> locations = addLocation();
+        String city = locations.stream().map(LocationDto::city).findAny().orElse("");
+        String country = locations.stream().map(LocationDto::country).findAny().orElseThrow();
+
+        WeatherResponseDto weatherResponseDto =  windsurferWeatherClient.getWeatherForLocation(city,country);
         log.info(weatherResponseDto);
         return weatherResponseDto;
+    }
+
+    ResponseEntity<?> valid(String date) {
+        requireNonNull(date);
+        LocalDate dateWeather;
+        try {
+           dateWeather = LocalDate.parse(date);
+        } catch (DateTimeParseException e) {
+           return ResponseEntity.badRequest().body(null);
+        }
+        return ResponseEntity.ok(dateWeather);
     }
 
     private Set<LocationDto> addLocation() {
@@ -38,8 +58,8 @@ class WeatherServiceImpl implements WeatherService {
         LocationDto bridgetown = new LocationDto("Bridgetown", "Barbados");
         LocationDto fortaleza = new LocationDto("Fortaleza", "Brazylia");
         LocationDto pissouri = new LocationDto("Pissouri", "Cyprus");
-        LocationDto le_Monre = new LocationDto("Le Monre", "Mauritius");
-        return Set.of(jastarnia, bridgetown, fortaleza, pissouri, le_Monre);
+        LocationDto le_monre = new LocationDto("Le Monre", "Mauritius");
+        return Set.of(jastarnia, bridgetown, fortaleza, pissouri, le_monre);
     }
 
     LocationDto addLocation(String city, String country) {
