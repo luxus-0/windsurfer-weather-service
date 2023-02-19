08d22097973dfd8253d3d6dda53b09ef44a04503
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-19 16:38:51 +0100

    Change method: HTTP GET  1.ResponseEntity<WeatherResponseDto>readWindsurfingLocation(@PathVariable String date);
     With @RequestParam city, country, date
     return client.getWeatherLocation(city, country, date)
    
     HTTP POST
     2.ResponseEntity<LocationDto> addLocation(@PathVariable String city, @PathVariable String country)
     return weatherService.addLocation(city, country);
    
     Save to database

--- src/main/java/com/github/windurferweather/weather/WindSurferWeatherApi.java
+++ src/main/java/com/github/windurferweather/weather/WindSurferWeatherApi.java
@@ -1,32 +1,40 @@
 package com.github.windurferweather.weather;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 class WindSurferWeatherApi {
 
-    private final WeatherServiceImpl windSurferServiceImpl;
+    private final WeatherClient weatherClient;
+    private final WeatherServiceImpl weatherService;
 
-    WindSurferWeatherApi(WeatherServiceImpl windSurferServiceImpl) {
-        this.windSurferServiceImpl = windSurferServiceImpl;
+    WindSurferWeatherApi(WeatherClient weatherClient, WeatherServiceImpl weatherService) {
+        this.weatherClient = weatherClient;
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/windsurfing_location")
+    ResponseEntity<WeatherResponseDto> readWindsurfingLocation(@RequestParam String city, @RequestParam String country, @RequestParam String date) throws Exception {
+        WeatherResponseDto weatherForLocation = weatherClient.getWeatherForLocation(city, country, date);
+        if(weatherForLocation != null){
+            return ResponseEntity.ok(weatherForLocation);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/windsurfing_location/{date}")
-    ResponseEntity<WeatherResponseDto> readWindsurfingLocation(@PathVariable String date) {
-        WeatherResponseDto bestLocationForWindsurfing = windSurferServiceImpl.readWindsurfingLocation(date);
+    ResponseEntity<LocationDto> readBestLocationForWindsurfingByDate(@PathVariable String date) throws Exception {
+        LocationDto bestLocationForWindsurfing = weatherService.readWindsurfingLocation(date);
         if(bestLocationForWindsurfing != null){
             return ResponseEntity.ok(bestLocationForWindsurfing);
         }
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping("/create_location_weather/{city}/{country}")
+    @PostMapping("/add_location_weather/{city}/{country}")
     ResponseEntity<LocationDto> addLocation(@PathVariable String city, @PathVariable String country) {
-        LocationDto location = windSurferServiceImpl.addLocation(city, country);
+        LocationDto location = weatherService.addLocation(city, country);
         if (location != null) {
             return ResponseEntity.ok().body(location);
         }
