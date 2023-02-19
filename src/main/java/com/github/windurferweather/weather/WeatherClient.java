2a0deb12d6dc026288434533065e3c8d8016a522
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-19 22:10:16 +0100

    Reformat code

--- src/main/java/com/github/windurferweather/weather/WeatherClient.java
+++ src/main/java/com/github/windurferweather/weather/WeatherClient.java
@@ -1,15 +1,11 @@
 package com.github.windurferweather.weather;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.net.URL;
 
-import static com.github.windurferweather.weather.WeatherConditionMessage.TEMPERATURE;
-import static com.github.windurferweather.weather.WeatherConditionMessage.WIND_SPEED;
 import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.API_KEY;
 import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.WEATHER_URL;
 
@@ -18,27 +14,19 @@ import static com.github.windurferweather.weather.WindsurferWeatherMessageProvid
 class WeatherClient {
 
     WeatherResponseDto getWeatherForLocation(String city, String country, String localDateTime) throws Exception {
-        JsonNode json = convertToJsonWeather(city, country, localDateTime);
 
-        double windSpeed = json.get(WIND_SPEED).asDouble();
-        double temperature = json.get(TEMPERATURE).asDouble();
+        ObjectMapper mapper = new ObjectMapper();
+        WeatherResponseDto weatherResponseDto = mapper.readValue(new URL(WEATHER_URL + "daily?city=" + city + "&country=" + country + "&datetime=" + localDateTime + "&key=" + API_KEY), WeatherResponseDto.class);
 
-        WeatherConditionDto weatherConditionDto = new WeatherConditionDto(windSpeed, temperature);
-        LocationDto location = new LocationDto(city, country);
+        double temperature = weatherResponseDto.temp();
+       double windSpeed = weatherResponseDto.windSpeed();
 
         return WeatherResponseDto.builder()
-                .weatherConditionDto(weatherConditionDto)
-                .locationDto(location)
+                .city(city)
+                .country(country)
+                .windSpeed(windSpeed)
+                .temp(temperature)
                 .date(localDateTime)
                 .build();
-    }
-
-    private JsonNode convertToJsonWeather(String city, String country, String date) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(new URL(WEATHER_URL + "daily?city=" + city + "&country=" + country + "&datetime=" + date + "&key=" + API_KEY));
-        JsonNode data = root.get("data");
-        JsonNode day = data.get(0);
-        log.info(day);
-        return day;
     }
 }
