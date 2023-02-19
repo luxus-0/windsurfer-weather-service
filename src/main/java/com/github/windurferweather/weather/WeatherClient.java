7d588ccec0e66c0675226910e2254a6babb7610b
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-19 16:15:05 +0100

    Change from RestTemplate client To convert From URL to Json Weather with return WeatherResponseDto

--- src/main/java/com/github/windurferweather/weather/WeatherClient.java
+++ src/main/java/com/github/windurferweather/weather/WeatherClient.java
@@ -1,9 +1,23 @@
 package com.github.windurferweather.weather;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+import static com.github.windurferweather.weather.WeatherConditionMessage.*;
 import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.API_KEY;
 import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.WEATHER_URL;
 
@@ -11,9 +25,28 @@ import static com.github.windurferweather.weather.WindsurferWeatherMessageProvid
 @Log4j2
 class WeatherClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    WeatherResponseDto getWeatherForLocation(String city, String country) {
-        return restTemplate.getForObject(WEATHER_URL + "daily?city={city}&country={country}&key={API_KEY}",
-                WeatherResponseDto.class, city, country, API_KEY);
+    WeatherResponseDto getWeatherForLocation(String city, String country, String localDateTime) throws Exception {
+        JsonNode json = convertToJsonWeather(city, country, localDateTime);
+
+        double windSpeed = json.get(WIND_SPEED).asDouble();
+        double temperature = json.get(TEMPERATURE).asDouble();
+
+        WeatherConditionDto weatherConditionDto = new WeatherConditionDto(windSpeed, temperature);
+        LocationDto location = new LocationDto(city, country);
+
+        return WeatherResponseDto.builder()
+                .weatherConditionDto(weatherConditionDto)
+                .locationDto(location)
+                .date(localDateTime)
+                .build();
+    }
+
+    private JsonNode convertToJsonWeather(String city, String country, String date) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(new URL(WEATHER_URL + "daily?city=" + city + "&country="+ country + "&datetime=" + date + "&key=" + API_KEY));
+        JsonNode data = root.get("data");
+        JsonNode day = data.get(0);
+        log.info(day);
+        return day;
     }
 }
