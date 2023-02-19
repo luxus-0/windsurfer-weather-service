773208a770ac08deedc180b7a521c6e2e128ecbb
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-19 22:10:35 +0100

    Reformat code

---
+++ src/main/java/com/github/windurferweather/weather/BestLocationWeatherForWindsurferMessage.java
@@ -0,0 +1,14 @@
+package com.github.windurferweather.weather;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+class BestLocationWeatherForWindsurferMessage {
+    static void showBestLocationForWindsurfer(WeatherResponseDto bestLocationForWindsurfing) {
+        log.info("\nBest location for windsurfing in Date: " + bestLocationForWindsurfing.date() + "\n" +
+                "City: " +bestLocationForWindsurfing.city() + "\n" +
+                "Country: " +bestLocationForWindsurfing.country() + "\n" +
+                "WindSpeed: " +bestLocationForWindsurfing.windSpeed() + "\n" +
+                "Temperature: " + bestLocationForWindsurfing.temp() + "\n");
+    }
+}
