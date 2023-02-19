d85a428d8d7ce2470f7129beec45774377e96c75
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-19 16:21:57 +0100

    Change in WeatherResponseDto fields: String city, String country and double windSpeed, double temp
    Change to DTO
    city, country -> LocationDto
    windSpeed, temp -> WeatherConditionDto

--- src/main/java/com/github/windurferweather/weather/WeatherResponseDto.java
+++ src/main/java/com/github/windurferweather/weather/WeatherResponseDto.java
@@ -3,5 +3,5 @@ package com.github.windurferweather.weather;
 import lombok.Builder;
 
 @Builder
-public record WeatherResponseDto(String city, String country, double windSpeed, double temperature, String date) {
+public record WeatherResponseDto(LocationDto locationDto, WeatherConditionDto weatherConditionDto, String date) {
 }
