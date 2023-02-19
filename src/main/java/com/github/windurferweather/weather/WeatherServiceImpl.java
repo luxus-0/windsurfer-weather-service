a540f0527e6874ae40cdd5ae002e8f294572fd9b
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-19 16:31:04 +0100

    Refactor code in method WeatherResponseDto readWindsurfingLocation(String date)
    1.Read locations: Jastarnia, Bridgetown, Fortaleza, Pissouri, Le Monre
    2.Read city
    3.Read country
    4.Read from client conversion to JSON: wind speed and temperature
    5.filter by best condition for wind and temp. After that comparing best score: (windSpeed * 3) + temp;
    6.After finding max number than return Location with city and country

--- src/main/java/com/github/windurferweather/weather/WeatherServiceImpl.java
+++ src/main/java/com/github/windurferweather/weather/WeatherServiceImpl.java
@@ -1,14 +1,16 @@
 package com.github.windurferweather.weather;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.*;
 import static java.util.Objects.requireNonNull;
@@ -19,47 +21,68 @@ class WeatherServiceImpl implements WeatherService {
     private final WindsurferWeatherRepository windsurferWeatherRepository;
     private final WeatherClient windsurferWeatherClient;
 
-    @Value("${weather.url}")
-    private String url;
-
-    @Value("${weather.apiKey}")
-    private String apiKey;
-
     WeatherServiceImpl(WindsurferWeatherRepository windsurferWeatherRepository, WeatherClient windsurferWeatherClient) {
         this.windsurferWeatherRepository = windsurferWeatherRepository;
         this.windsurferWeatherClient = windsurferWeatherClient;
     }
 
     @Override
-    public WeatherResponseDto readWindsurfingLocation(String date) {
+    public LocationDto readWindsurfingLocation(String date) throws Exception {
         valid(date);
-        Set<LocationDto> locations = addLocation();
-        String city = locations.stream().map(LocationDto::city).findAny().orElse("");
-        String country = locations.stream().map(LocationDto::country).findAny().orElseThrow();
+        List<LocationDto> locations = getLocations();
 
-        WeatherResponseDto weatherResponseDto =  windsurferWeatherClient.getWeatherForLocation(city,country);
-        log.info(weatherResponseDto);
-        return weatherResponseDto;
+        String city = readCity(locations);
+        String country = readCountry(locations);
+
+        WeatherResponseDto weatherResponseDto = windsurferWeatherClient.getWeatherForLocation(city, country, date);
+        double windSpeed = weatherResponseDto.weatherConditionDto().getWindSpeed();
+        double temp = weatherResponseDto.weatherConditionDto().getTemperature();
+
+        Stream.of(weatherResponseDto)
+                .filter(findBestConditionByWindAndTemp -> checkBestConditionForWindSurfer(windSpeed, temp))
+                .max(Comparator.comparingDouble(checkBestWeather -> calculateBestWeatherForWindsurfing(windSpeed, temp)))
+                .stream()
+                .findAny()
+                .ifPresent(showLocation -> new LocationDto(city, country));
+
+        return new LocationDto("","");
+    }
+
+    private String readCountry(List<LocationDto> locations) {
+        return locations
+                .stream()
+                .map(LocationDto::country)
+                .findAny()
+                .orElse("");
+    }
+
+    private String readCity(List<LocationDto> locations) {
+        return locations
+                .stream()
+                .map(LocationDto::city)
+                .findAny()
+                .orElse("");
+    }
+
+    private List<LocationDto> getLocations() {
+        return List.of(
+                new LocationDto("Jastarnia", "Polska"),
+                new LocationDto("Bridgetown", "Barbarados"),
+                new LocationDto("Fortaleza", "Brazylia"),
+                new LocationDto("Pissouri", "Cypr"),
+                new LocationDto("Le Monre", "Mauritius"));
+
     }
 
     ResponseEntity<?> valid(String date) {
         requireNonNull(date);
         LocalDate dateWeather;
         try {
-           dateWeather = LocalDate.parse(date);
+            dateWeather = LocalDate.parse(date);
         } catch (DateTimeParseException e) {
-           return ResponseEntity.badRequest().body(null);
+            return ResponseEntity.badRequest().body(null);
         }
         return ResponseEntity.ok(dateWeather);
-    }
-
-    private Set<LocationDto> addLocation() {
-        LocationDto jastarnia = new LocationDto("Jastarnia", "Poland");
-        LocationDto bridgetown = new LocationDto("Bridgetown", "Barbados");
-        LocationDto fortaleza = new LocationDto("Fortaleza", "Brazylia");
-        LocationDto pissouri = new LocationDto("Pissouri", "Cyprus");
-        LocationDto le_monre = new LocationDto("Le Monre", "Mauritius");
-        return Set.of(jastarnia, bridgetown, fortaleza, pissouri, le_monre);
     }
 
     LocationDto addLocation(String city, String country) {
@@ -69,23 +92,20 @@ class WeatherServiceImpl implements WeatherService {
 
         WindSurferWeather localizationWeatherSaved = windsurferWeatherRepository.save(localization);
 
-        return getLocation(city, country, localizationWeatherSaved);
-    }
-
-    private static LocationDto getLocation(String city, String country, WindSurferWeather localizationWeatherSaved) {
-        return Set.of(localizationWeatherSaved).stream()
+        return Set.of(localizationWeatherSaved)
+                .stream()
                 .filter(Objects::nonNull)
                 .map(toDto -> new LocationDto(city, country))
                 .findAny()
-                .orElse(new LocationDto("", ""));
+                .orElse(new LocationDto("Empty city", "Empty country"));
     }
 
-    boolean checkBestPlaceForWindSurfer(double windSpeed, double temperature) {
+    boolean checkBestConditionForWindSurfer(double windSpeed, double temperature) {
         return windSpeed >= MIN_WIND && windSpeed <= MAX_WIND &&
                 temperature >= MIN_TEMP && temperature <= MAX_TEMP;
     }
 
-    double findBestLocalizationForSurfer(double windSpeed, double temp) {
+    double calculateBestWeatherForWindsurfing(double windSpeed, double temp) {
         return (windSpeed * 3) + temp;
     }
 }
