9236d7abc3415ea14dc737d48f6c1d0b45eb15c1
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-20 23:39:50 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/WeatherConditionMessage.java
+++
@@ -1,6 +0,0 @@
-package com.github.windurferweather.weather;
-
-class WeatherConditionMessage {
-    public static final String WIND_SPEED = "wind_spd";
-    public static final String TEMPERATURE = "temp";
-}
