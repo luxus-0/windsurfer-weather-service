aeaced76cb729c6554c9c738b528afe834e76427
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-20 23:40:19 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/BestLocationWeatherForWindsurferMessage.java
+++ src/main/java/com/github/windurferweather/weather/BestLocationWeatherForWindsurferMessage.java
@@ -4,7 +4,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 class BestLocationWeatherForWindsurferMessage {
-    static void showBestLocationForWindsurfer(WeatherResponseDto bestLocationForWindsurfing) {
+    static void showBestLocationForWindsurfer(WeatherConditionDto bestLocationForWindsurfing) {
         log.info("\nBest location for windsurfing in Date: " + bestLocationForWindsurfing.date() + "\n" +
                 "City: " +bestLocationForWindsurfing.city() + "\n" +
                 "Country: " +bestLocationForWindsurfing.country() + "\n" +
--- src/main/java/com/github/windurferweather/weather/LocationDto.java
+++
@@ -1,4 +0,0 @@
-package com.github.windurferweather.weather;
-
-public record LocationDto(String city, String country) {
-}
