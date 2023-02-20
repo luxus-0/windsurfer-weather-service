65bfaa6ea7d55cfdfa7b28e6b5d1a79c1a98183f
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-20 23:39:44 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/WeatherClient.java
+++ src/main/java/com/github/windurferweather/weather/WeatherClient.java
@@ -1,32 +1,28 @@
 package com.github.windurferweather.weather;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
-import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.API_KEY;
-import static com.github.windurferweather.weather.WindsurferWeatherMessageProvider.WEATHER_URL;
+import static com.github.windurferweather.weather.WeatherApiKey.API_KEY;
+import static com.github.windurferweather.weather.WeatherEndpoint.ENDPOINT;
 
 @Service
 @Log4j2
 class WeatherClient {
 
-    WeatherResponseDto getWeatherForLocation(String city, String country, String localDateTime) throws Exception {
+    private final RestTemplate restTemplate = new RestTemplate();
 
-        ObjectMapper mapper = new ObjectMapper();
-        WeatherResponseDto weatherResponseDto = mapper.readValue(new URL(WEATHER_URL + "daily?city=" + city + "&country=" + country + "&datetime=" + localDateTime + "&key=" + API_KEY), WeatherResponseDto.class);
-
-        double temperature = weatherResponseDto.temp();
-       double windSpeed = weatherResponseDto.windSpeed();
-
-        return WeatherResponseDto.builder()
-                .city(city)
-                .country(country)
-                .windSpeed(windSpeed)
-                .temp(temperature)
-                .date(localDateTime)
-                .build();
+    String getForecastWeather(String city) {
+        String url = ENDPOINT +"?city= " + city + "&key=" + API_KEY;
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        ResponseEntity<String> weatherResponse =  restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        return weatherResponse.getBody();
     }
 }
