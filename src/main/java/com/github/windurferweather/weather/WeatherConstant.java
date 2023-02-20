3fcfb96eeb1bcd1fbaf270440dc86437a0f8e69a
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-20 23:39:54 +0100

    Change

---
+++ src/main/java/com/github/windurferweather/weather/WeatherConstant.java
@@ -0,0 +1,8 @@
+package com.github.windurferweather.weather;
+
+public class WeatherConstant {
+    public static final int MIN_TEMP = 3;
+    public static final int MAX_TEMP = 35;
+    public static final int MIN_WIND = 5;
+    public static final int MAX_WIND = 18;
+}
