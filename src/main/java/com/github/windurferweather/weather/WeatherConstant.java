88561d64b075668fcd2fd02bbfe8d50b3b8dc723
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-21 20:33:13 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/WeatherConstant.java
+++ src/main/java/com/github/windurferweather/weather/WeatherConstant.java
@@ -1,6 +1,10 @@
 package com.github.windurferweather.weather;
 
+import static com.github.windurferweather.weather.WeatherEndpoint.ENDPOINT;
+
 class WeatherConstant {
+
+    public static final String API_KEY = "273c07c4d603455cabd9f94cb58b526c";
     public static final int MIN_TEMP = 3;
     public static final int MAX_TEMP = 35;
     public static final int MIN_WIND = 5;
@@ -9,7 +13,9 @@ class WeatherConstant {
     public static final String WIND_SPEED = "wind_spd";
     public static final String TEMPERATURE = "temp";
     public static final String CITY = "city_name";
-    public static final String VALID_DATE = "valid_date";
-
     public static final String DATA = "data";
+
+    public static String getUrl(String city, String country) {
+        return ENDPOINT +"?city=" + city + "&country=" + country + "&key=" + API_KEY;
+    }
 }
--- src/main/java/com/github/windurferweather/weather/WeatherEndpoint.java
+++ src/main/java/com/github/windurferweather/weather/WeatherEndpoint.java
@@ -1,5 +1,5 @@
 package com.github.windurferweather.weather;
 
 class WeatherEndpoint {
-    public static final String ENDPOINT = "https://api.weatherbit.io/v2.0/forecast/daily";
+    public static final String ENDPOINT = "http://api.weatherbit.io/v2.0/forecast/daily";
 }
