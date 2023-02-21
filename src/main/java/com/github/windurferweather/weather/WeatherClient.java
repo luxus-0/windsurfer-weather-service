cc9aa9feb7ec03e93b0c64473ac1290ca765c8d5
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-21 20:33:18 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/WeatherClient.java
+++ src/main/java/com/github/windurferweather/weather/WeatherClient.java
@@ -1,28 +1,18 @@
 package com.github.windurferweather.weather;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static com.github.windurferweather.weather.WeatherApiKey.API_KEY;
-import static com.github.windurferweather.weather.WeatherEndpoint.ENDPOINT;
+import static com.github.windurferweather.weather.WeatherConstant.getUrl;
 
 @Service
 @Log4j2
 class WeatherClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    String getForecastWeather(String city) {
-        String url = ENDPOINT +"?city= " + city + "&key=" + API_KEY;
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        ResponseEntity<String> weatherResponse =  restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        return weatherResponse.getBody();
+    WeatherResponseDto getForecastWeather(String city, String country_code) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = getUrl(city, country_code);
+        return restTemplate.getForObject(url, WeatherResponseDto.class, city, country_code);
     }
 }
--- src/main/java/com/github/windurferweather/weather/WeatherConditionDto.java
+++
@@ -1,4 +0,0 @@
-package com.github.windurferweather.weather;
-
-public record WeatherConditionDto(String location, double windSpeed, double temperature, String date) {
-}
