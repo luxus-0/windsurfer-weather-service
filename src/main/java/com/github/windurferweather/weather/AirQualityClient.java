04e99c4af97515e3f2262249c4148ae005cf8ad4
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-23 10:26:02 +0100

    Method search air quality by city: AirQuality(String city);

--- src/main/java/com/github/windurferweather/weather/AirQualityClient.java
+++ src/main/java/com/github/windurferweather/weather/AirQualityClient.java
@@ -1,19 +1,20 @@
 package com.github.windurferweather.weather;
 
+import com.github.windurferweather.config.ConfigurationClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import static com.github.windurferweather.weather.AirQualityConstant.AIR_QUALITY_URL;
 
 @Service
 class AirQualityClient {
-    private final RestTemplate restTemplate;
+    private final ConfigurationClient client;
 
-    AirQualityClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    AirQualityClient(ConfigurationClient client) {
+        this.client = client;
     }
 
     AirQualityDto readAirQuality(String city) {
-        return restTemplate.getForObject(AIR_QUALITY_URL, AirQualityDto.class, city);
+        return client.getRestTemplate()
+                .getForObject(AIR_QUALITY_URL, AirQualityDto.class, city);
     }
 }
