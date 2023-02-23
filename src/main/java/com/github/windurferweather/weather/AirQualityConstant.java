962262b14f4327ff55c85e8721e2692d3ce05b5d
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-23 09:27:06 +0100

    Add constants for air quality url: city, api_key, air_quality_url

---
+++ src/main/java/com/github/windurferweather/weather/AirQualityConstant.java
@@ -0,0 +1,8 @@
+package com.github.windurferweather.weather;
+
+class AirQualityConstant {
+    public static final String CITY = "?city={city}";
+
+    public static final String API_KEY = "273c07c4d603455cabd9f94cb58b526c";
+    public static final String AIR_QUALITY_URL = "https://api.weatherbit.io/v2.0/current/airquality" + CITY + "&key=" + API_KEY;
+}
