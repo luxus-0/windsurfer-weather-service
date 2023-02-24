d422a64e1189450e10efdaf57cf743decfe2e347
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-24 16:08:20 +0100

    Change

--- src/main/java/com/github/windurferweather/weather/Data.java
+++ src/main/java/com/github/windurferweather/weather/Data.java
@@ -1,4 +1,21 @@
 package com.github.windurferweather.weather;
 
-record Data(int aqi) {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Entity
+@Getter
+@AllArgsConstructor
+class Data {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int aqi;
+    public Data() {
+    }
+
+    public Data(int aqi) {
+        this.aqi = aqi;
+    }
 }
