922794083aec300b7b6415122532489901742861
Łukasz Nowogórski <luxus0@gmail.com>
2023-02-24 14:40:07 +0100

    Add static method getAirQualityUrl(city)

--- src/main/java/com/github/windurferweather/weather/AirQualityConstant.java
+++ src/main/java/com/github/windurferweather/weather/AirQualityConstant.java
@@ -1,8 +1,13 @@
 package com.github.windurferweather.weather;
 
-class AirQualityConstant {
-    public static final String CITY = "?city={city}";
+import static com.github.windurferweather.weather.WeatherEndpoint.ENDPOINT;
 
+class AirQualityConstant {
     public static final String API_KEY = "273c07c4d603455cabd9f94cb58b526c";
-    public static final String AIR_QUALITY_URL = "https://api.weatherbit.io/v2.0/current/airquality" + CITY + "&key=" + API_KEY;
+    public static final String AIR_QUALITY_URL = "http://api.weatherbit.io/v2.0/current/airquality";
+    public static final String DATA = "data";
+
+    public static String getAirQualityUrl(String city) {
+        return AIR_QUALITY_URL +"?city=" + city + "&key=" + API_KEY;
+    }
 }
