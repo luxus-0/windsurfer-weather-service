59bdfbb20529b55fec8b1b17558a40363558a6d7
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-01 00:11:15 +0100

    Remove unused class

--- src/main/java/com/github/windurferweather/weather/AirQualityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.windurferweather.weather;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirQualityRepository extends JpaRepository<AirQuality, Long> {
-}
