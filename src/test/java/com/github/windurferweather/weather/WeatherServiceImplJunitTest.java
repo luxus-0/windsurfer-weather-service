68334553735056322eb200d8d5b7274f50199904
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-03 21:51:47 +0100

    Repair test methods

--- src/test/java/com/github/windurferweather/weather/WeatherServiceImplJunitTest.java
+++ src/test/java/com/github/windurferweather/weather/WeatherServiceImplJunitTest.java
@@ -5,10 +5,24 @@ import com.github.windurferweather.weather.dto.WeatherResponseDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-class WeatherServiceImplTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class WeatherServiceImplJunitTest {
     private final WeatherClient weatherClient = new WeatherClient();
     private final WeatherServiceImpl weatherService = new WeatherServiceImpl(weatherClient);
+    private final MockMvc mockMvc;
+
+    WeatherServiceImplJunitTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
 
     @Test
     @DisplayName("Should return incorrect city when user gave date")
