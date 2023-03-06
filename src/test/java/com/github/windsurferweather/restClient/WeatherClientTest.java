dd9cf2d3db8391d1e151d95831ff69b43a267e43
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-06 23:23:59 +0100

    Add test cases to method getForecastWeather(city, country, date)

---
+++ src/test/java/com/github/windsurferweather/restClient/WeatherClientTest.java
@@ -0,0 +1,63 @@
+package com.github.windsurferweather.restClient;
+
+import com.github.windsurferweather.model.Weather;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+import static com.github.windsurferweather.utils.Tools.getToday;
+import static java.time.LocalDate.now;
+
+@SpringBootTest
+class WeatherClientTest {
+
+   private final WeatherClient weatherClient = new WeatherClient();
+
+    @Test
+    void shouldReturnForecastWeather(){
+        String city = "Jastarnia";
+        String country = "pl";
+
+        Weather forecastWeather = weatherClient.getForecastWeather(city, country, getToday());
+
+        Assertions.assertThat(forecastWeather).isNotNull();
+    }
+
+    @Test
+    void shouldReturnForecastWeatherWhenIncorrectDate(){
+        String city = "Bridgetown";
+        String country = "BB";
+        String today = now().minus(2, ChronoUnit.DAYS).toString();
+
+        Weather forecastWeather = weatherClient.getForecastWeather(city, country, today);
+
+        Assertions.assertThat(forecastWeather).isNotNull();
+        Assertions.assertThat(today).isNotEqualTo(getToday());
+    }
+
+    @Test
+    void shouldReturnForecastWeatherWhenCorrectDate(){
+        String city = "Bridgetown";
+        String country = "BB";
+        String today = String.valueOf(LocalDate.now());
+
+        Weather forecastWeather = weatherClient.getForecastWeather(city, country, today);
+
+        Assertions.assertThat(forecastWeather).isNotNull();
+        Assertions.assertThat(today).isEqualTo(getToday());
+    }
+
+    @Test
+    void shouldReturnForecastWeatherWhenCityAndCountryIsInCorrect(){
+
+        Weather forecastWeather = weatherClient.getForecastWeather("Warsaw", "PL", getToday());
+
+        Assertions.assertThat(forecastWeather).isNotNull();
+        Assertions.assertThat(forecastWeather.getCityName()).isNotEqualTo("Fortaleza");
+        Assertions.assertThat(forecastWeather.getCountryCode()).isNotEqualTo("BR");
+    }
+}
