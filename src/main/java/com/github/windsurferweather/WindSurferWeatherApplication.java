ad093282bb86131b6f25202cd63450f3ad3a3800
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-06 22:23:44 +0100

    Change

--- src/main/java/com/github/windsurferweather/WindSurferWeatherApplication.java
+++ src/main/java/com/github/windsurferweather/WindSurferWeatherApplication.java
@@ -1,4 +1,4 @@
-package com.github.windurferweather;
+package com.github.windsurferweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
