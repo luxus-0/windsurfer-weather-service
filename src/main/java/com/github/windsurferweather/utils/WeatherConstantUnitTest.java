f6f7df26443fb82247e352ef97486e26f975e9b1
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-11 23:24:30 +0100

    Add private static method from used in test: WeatherServiceTest and WeatherServiceParametrizedTest

--- src/main/java/com/github/windsurferweather/utils/WeatherConstantUnitTest.java
+++ src/main/java/com/github/windsurferweather/utils/WeatherConstantUnitTest.java
@@ -3,7 +3,9 @@ package com.github.windsurferweather.utils;
 import com.github.windsurferweather.model.Data;
 import com.github.windsurferweather.model.Weather;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.stream.Stream;
 
 public class WeatherConstantUnitTest {
@@ -43,10 +45,38 @@ public class WeatherConstantUnitTest {
                 .orElse(0d);
     }
 
-    public static Double getActualWindSpeed(Weather actualWeather) {
-        return actualWeather.getData().stream()
-                .map(Data::getWindSpeed).findAny()
+    public static double getActualWindSpeed(Weather expectedWeather) {
+        return expectedWeather.getData().stream()
+                .mapToDouble(Data::getWindSpeed)
+                .findAny()
                 .orElse(0d);
     }
 
+    public static Double readWindSpeed(List<Weather> actualWeather) {
+        return actualWeather.stream()
+                .map(Weather::getData)
+                .flatMap(Collection::stream)
+                .map(Data::getWindSpeed)
+                .findAny()
+                .orElseThrow();
+    }
+    public static Double readMaxTemperature(List<Weather> actualWeather) {
+        return actualWeather.listIterator()
+                .next()
+                .getData()
+                .stream()
+                .mapToDouble(Data::getMaxTemperature)
+                .iterator()
+                .next();
+    }
+
+    public static Double readMinTemperature(List<Weather> actualWeathers) {
+        for(Weather weather : actualWeathers){
+            ListIterator<Data> weatherIterator = weather.getData().listIterator();
+            if(weatherIterator.hasNext()){
+                return weatherIterator.next().getMinTemperature();
+            }
+        }
+        return 0d;
+    }
 }
