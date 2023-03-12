ed5cd0c0c732c0998becadae763debd7cd5de223
Łukasz Nowogórski <luxus0@gmail.com>
2023-03-12 22:48:32 +0100

    Change parameter method:
    List<WeatherReport> findAllByCityAndCountryAndStartAndEndBetween(String city, String country, LocalDateTime startTime, LocalDateTime endTime);
    
    From LocalDateTime to LocalTime because we used only time.

--- src/main/java/com/github/windsurferweather/repository/WeatherReportRepository.java
+++ src/main/java/com/github/windsurferweather/repository/WeatherReportRepository.java
@@ -3,10 +3,10 @@ package com.github.windsurferweather.repository;
 import com.github.windsurferweather.model.WeatherReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface WeatherReportRepository extends JpaRepository<WeatherReport, Long> {
     List<WeatherReport> findAllByCityAndCountry(String city, String country);
-    List<WeatherReport> findAllByCityAndCountryAndStartAndEndBetween(String city, String country, LocalDateTime startTime, LocalDateTime endTime);
+    List<WeatherReport> findAllByCityAndCountryAndStartAndEndBetween(String city, String country, LocalTime startTime, LocalTime endTime);
 }
